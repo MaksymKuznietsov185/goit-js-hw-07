1478f520d5a848ea4e76226aa7138eaab3264d93
Maksym Kuznietsov <maksimmanzar4@gmail.com>
2025-12-11 22:02:27 +0100

    message

--- js/task-1.js
+++ js/task-1.js
@@ -5,7 +5,7 @@ console.log("Number of categories:", items.length);
 
 items.forEach((item) => {
   const title = item.querySelector("h2").textContent;
-  const elementsCount = item.querySelectorAll("li").length;
+  const elementsCount = item.querySelectorAll("ul li").length;
 
   console.log("Category:", title);
   console.log("Elements:", elementsCount);
